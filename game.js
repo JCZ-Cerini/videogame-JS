22ffeb65bebe7a9b74209b328eb22c6dbed2f88e
jczcerini <juancruzcerini@hotmail.com>
2023-01-03 18:49:15 -0300

    Renderizacion de mapas

--- game.js
+++ game.js
@@ -13,8 +13,15 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    for (let i = 1; i <= 10; i++) {        
-        game.fillText(emojis['X'], elementsSize, elementsSize * i);
+    const map = maps[0];
+    const mapsRows = map.trim().split('\n');
+    const mapRowCols = mapsRows.map(row => row.trim().split(''));
+    console.log({map, mapsRows, mapRowCols});
+
+    for (let row = 1; row <= 10; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+        }
     }
 }
 
